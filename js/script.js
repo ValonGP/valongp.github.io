95bbf60f259850d481f2cd0a73f779513b421a74
BuildTools <unconfigured@null.spigotmc.org>
2018-09-11 22:36:00 +0100

    visual update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 var map = null;
   
 function initialize() {
-
 	map = new google.maps.Map(document.getElementById('map'), {
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 		gestureHandling: 'greedy',
@@ -10,7 +9,6 @@ function initialize() {
 		zoomControl: false,
 		mapTypeControl: false,
 	});
-
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
